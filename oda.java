fb884d26b1b174fbdbca010ebecb0227cc4fe3de
Saksis <31075495+Saksis@users.noreply.github.com>
2017-08-16 12:04:01 -0500

    Add files via upload

---
+++ oda.java
@@ -0,0 +1,18 @@
+public class Oda {
+
+		int Rank;
+
+	public static void main(String[] args) {
+		Oda nobunaga = new Oda();
+	  nobunaga.go();
+
+	}
+
+	public void go() {
+    Rank = 5;
+		System.out.println(Rank);
+	}
+
+
+
+}
